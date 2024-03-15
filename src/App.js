057617646af0eced17c120dc38045da6ed6a8f9c
Parker Henderson <parker@parkerhendo.com>
2024-03-15 10:17:45 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <Button colorScheme="teal">Button</Button>
+      <Text fontSize="xl">Text</Text>
+    </div>
+  );
+}
+
+export default App;
